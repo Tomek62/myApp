8b2f1e115533a635990295115c20ee8d51800ffa
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-05-26 14:23:25 +0200

    FIX format

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,12 @@
-import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -23,7 +29,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   // Charger le token stocké au démarrage de l'application
   useEffect(() => {
     const loadToken = async () => {
-      const storedToken = await AsyncStorage.getItem('token');
+      const storedToken = await AsyncStorage.getItem("token");
       if (storedToken) {
         setToken(storedToken);
         setIsAuthenticated(true);
@@ -36,19 +42,21 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const signup = async (pseudo: string, email: string, password: string) => {
     try {
       console.log("Envoi de la requête :", { pseudo, email, password });
-      const response = await axios.post('http://172.20.10.2:5000/auth/register', {
-        pseudo,
-        email,
-        password,
-      });
+      const response = await axios.post(
+        `http://172.20.10.2:5000/auth/register`,
+        {
+          pseudo,
+          email,
+          password,
+        }
+      );
 
       // Auto-login après l'inscription
-      await AsyncStorage.setItem('token', response.data.token);
-      await AsyncStorage.setItem('user', JSON.stringify(response.data.user));
+      await AsyncStorage.setItem("token", response.data.token);
+      await AsyncStorage.setItem("user", JSON.stringify(response.data.user));
       setToken(response.data.token);
       setIsAuthenticated(true);
       console.log("Réponse de l'API:", response.data);
-      
     } catch (error: any) {
       if (error.response) {
         console.error("Erreur API :", error.response.data);
@@ -67,17 +75,17 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   // **Fonction Login**
   const login = async (email: string, password: string) => {
     try {
-      const response = await axios.post('http://172.20.10.2:5000/auth/login', {
+      const response = await axios.post("http://172.20.10.2:5000/auth/login", {
         email,
         password,
       });
 
       // Stockage du token
-      await AsyncStorage.setItem('token', response.data.token);
+      await AsyncStorage.setItem("token", response.data.token);
       console.log("Token reçu :", response.data.token);
       console.log("User reçu :", response.data.user);
-      
-      await AsyncStorage.setItem('user', JSON.stringify(response.data.user));
+
+      await AsyncStorage.setItem("user", JSON.stringify(response.data.user));
       setToken(response.data.token);
       setIsAuthenticated(true);
     } catch (error: any) {
@@ -87,13 +95,15 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   // **Fonction Logout**
   const logout = async () => {
-    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem("token");
     setToken(null);
     setIsAuthenticated(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, token, login, signup, logout }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, token, login, signup, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -103,7 +113,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
