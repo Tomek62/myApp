dfd263c1afd3ffb26a4de82dcf64ef63a8db4e43
Thomas Lefebvre <thomaslfb3@gmail.com>
2025-05-27 17:46:38 +0200

    FEAT init of profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,34 +1,125 @@
-import { View, Text,Image,TouchableOpacity,StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useRouter } from 'expo-router';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { useAuth } from '@/context/AuthContext';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { useAuth } from "@/context/AuthContext";
+import { ThemedText } from "@/components/ThemedText";
+import { SubSection } from "@/components/SubSection";
+import { ThemedView } from "@/components/ThemedView";
+import { capitalizeAll } from "@/utils/utils";
+
 export default function ProfileScreen() {
   const router = useRouter();
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
   return (
-    <ParallaxScrollView headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-          headerImage={
-            <Image
-              source={require("@/assets/images/partial-react-logo.png")}
-              style={styles.reactLogo}
-            />
-          }>
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-xl text-white">Profil</Text>
-      <Image
-        source={require('@/assets/images/beer-person-logo.png')}
-        width={200}
-        height={200}
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
         />
-          <TouchableOpacity style={styles.signupButton} onPress={async () => {
-  await AsyncStorage.removeItem('token');
-  router.push('/login');
-  console.log("🚮 Token supprimé !");
-}}>
-  <Text>Supprimer le token</Text>
-</TouchableOpacity>
-    </View>
+      }
+    >
+      <View className="flex-1 justify-center items-center">
+        <ThemedText
+          type="subtitle"
+          style={{ marginBottom: 20, fontWeight: "bold" }}
+        >
+          Votre Profil
+        </ThemedText>
+        {/* <Image
+          source={require("@/assets/images/beer-person-logo.png")}
+          width={40}
+          height={40}
+        /> */}
+        <View style={styles.card}>
+          <ThemedText
+            type="defaultSemiBold"
+            style={{ marginBottom: 20, textAlign: "center" }}
+          >
+            {capitalizeAll("Informations générales")}
+          </ThemedText>
+          <SubSection title="Pseudo">
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "#FFB74D",
+                borderWidth: 1,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+              }}
+              value={user?.pseudo || "Votre pseudo ici"}
+              editable={false}
+              selectTextOnFocus={false}
+              autoCapitalize="words"
+              autoCorrect={false}
+              onChangeText={(text) => console.log(text)}
+            />
+          </SubSection>
+          <SubSection title="Email">
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "#FFB74D",
+                borderWidth: 1,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+              }}
+              value={user?.email || "Votre email ici"}
+              editable={false}
+              selectTextOnFocus={false}
+              autoCapitalize="words"
+              autoCorrect={false}
+              onChangeText={(text) => console.log(text)}
+            />
+          </SubSection>
+          <SubSection title="Mot de passe">
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: "#FFB74D",
+                borderWidth: 1,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+              }}
+              value={user?.password || "Votre email ici"}
+              editable={false}
+              selectTextOnFocus={false}
+              autoCapitalize="words"
+              autoCorrect={false}
+              onChangeText={(text) => console.log(text)}
+            />
+          </SubSection>
+          <TouchableOpacity
+            style={styles.signupButton}
+          onPress={async () => {
+            await AsyncStorage.removeItem("token");
+            router.push("/login");
+            console.log("🚮 Token supprimé !");
+          }}
+        >
+          <Text style={styles.signupButtonText}>Modifier mon profil</Text>
+        </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={styles.signupButton}
+          onPress={async () => {
+            await AsyncStorage.removeItem("token");
+            router.push("/login");
+            console.log("🚮 Token supprimé !");
+          }}
+        >
+          <Text style={styles.signupButtonText}>Se déconnecter</Text>
+        </TouchableOpacity>
+      </View>
     </ParallaxScrollView>
   );
 }
@@ -41,19 +132,30 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   signupButton: {
+    margin: "auto",
     width: "70%",
     height: 50,
-    backgroundColor: "#FFF",
+    backgroundColor: "#FFB74D",
     borderRadius: 25,
     justifyContent: "center",
     alignItems: "center",
-    // marginVertical: 10,
+    marginVertical: 12,
     borderWidth: 1,
     borderColor: "#FFB74D",
   },
   signupButtonText: {
     fontSize: 18,
-    color: "#FFB74D",
+    color: "#fff",
     fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    elevation: 5,
   },
 });
